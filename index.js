7a8afd3a64e70ee1840624c539c82f3b0d7e12b5
rudro987 <108304110+rudro987@users.noreply.github.com>
2023-06-25 21:21:12 +0600

    completed one round of play

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ function getComputerChoice(){
     return computerChoice[random];
 }
 
+function playRound(playerSelection, computerSelection){
+    playerSelection = prompt("Please choose 'Rock', 'Paper', 'Scissors': ");
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = getComputerChoice();
+    if(playerSelection === computerSelection){
+        return "Tie";
+    }else if(
+        (playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Paper" && computerSelection === "Rock") || 
+        (playerSelection === "Scissors" && computerSelection === "Paper")
+    ){
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }else{
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
