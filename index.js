9da57c52c01d2db15b685bd98a6f9fa0c5a40df4
rudro987 <108304110+rudro987@users.noreply.github.com>
2023-06-25 22:06:43 +0600

    completed full game

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let playerWin = 0;
+let computerWin = 0;
+let tie = 0;
 function getComputerChoice(){
     let computerChoice = ["Rock", "Paper", "Scissors"];
     let random = Math.floor(Math.random() * computerChoice.length);
@@ -5,19 +8,41 @@ function getComputerChoice(){
 }
 
 function playRound(playerSelection, computerSelection){
+    
     playerSelection = prompt("Please choose 'Rock', 'Paper', 'Scissors': ");
+    while(playerSelection === null) {
+        playerSelection = prompt("Please choose 'Rock', 'Paper', 'Scissors': ");
+    }
     playerSelection = playerSelection.toLowerCase();
     computerSelection = getComputerChoice();
+    computerSelection = computerSelection.toLowerCase();
+    console.log(playerSelection,computerSelection);
     if(playerSelection === computerSelection){
-        return "Tie";
+        tie++;
     }else if(
-        (playerSelection === "Rock" && computerSelection === "Scissors") ||
-        (playerSelection === "Paper" && computerSelection === "Rock") || 
-        (playerSelection === "Scissors" && computerSelection === "Paper")
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") || 
+        (playerSelection === "scissors" && computerSelection === "paper")
     ){
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+        playerWin++;
+        
     }else{
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+        computerWin++;
     }
 }
 
+function gamePlay(){
+    for(let i = 1; i <= 5; i++){
+        playRound();
+        console.log(playerWin, computerWin, tie);
+    }
+if(playerWin > computerWin){
+    return `You win! you scored ${playerWin} and computer scored ${computerWin}. you tied ${tie} times`;
+}else if(playerWin < computerWin){
+    return `You lose! you scored ${playerWin} and computer scored ${computerWin}. you tied ${tie} times`;
+}else{
+    return `Game tied! you scored ${playerWin} and computer scored ${computerWin}. you tied ${tie} times`;
+}
+}
+
+console.log(gamePlay());
